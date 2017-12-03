4d1ee60a2423a531a582e404b88492ba86a3d8b1
Personal <Personal@DESKTOP-RP14LQJ>
2017-12-02 20:07:36 -0600

    Uploading project

--- src/main/java/PruebaApp/App.java
+++ src/main/java/PruebaApp/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("HAfhjfhjhgjPPY");
+        System.out.println(App.SwichtNumbers(10, 0x33));
     }
+    public static String SwichtNumbers(int numberOne, int numberTwo)
+    {   
+        String result;
+        
+        System.out.print("Valores inciales, numero uno: " + numberOne + " numero dos: " + numberTwo + "\n");
+        numberTwo = numberOne + numberTwo;
+        numberOne = numberTwo - numberOne;
+        numberTwo = numberTwo - numberOne; 
+        result = "El valor del numero uno es: " + numberOne + " y el valor del numero dos es: " + numberTwo ; 
+        return result;
+        
+    }
+            
 }
