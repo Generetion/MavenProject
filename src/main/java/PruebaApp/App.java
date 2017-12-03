9bffea4f34e7d3a3aafa0c5def876f81d629f35a
Personal <Personal@DESKTOP-RP14LQJ>
2017-12-02 21:43:01 -0600

    Uploading project

--- src/main/java/PruebaApp/App.java
+++ src/main/java/PruebaApp/App.java
@@ -13,7 +13,7 @@ public class App
     public static String SwichtNumbers(int numberOne, int numberTwo)
     {   
         String result;
-        
+        String prueba = "Dios Rodi";
         System.out.print("Valores inciales, numero uno: " + numberOne + " numero dos: " + numberTwo + "\n");
         numberTwo = numberOne + numberTwo;
         numberOne = numberTwo - numberOne;
